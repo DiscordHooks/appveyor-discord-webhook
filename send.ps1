79e90797ff9fd31cbeacc5e41f293768db3929a3
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-04-09 11:43:16 +0000

    Added send powershell script

---
+++ send.ps1
@@ -0,0 +1,91 @@
+# Author: Sankarsan Kampa (a.k.a. k3rn31p4nic)
+# License: MIT
+
+$STATUS=$args[0]
+$WEBHOOK_URL=$args[1]
+
+if (!$WEBHOOK_URL) {
+  Write-Output "WARNING!!"
+  Write-Output "You need to pass the WEBHOOK_URL environment variable as the second argument to this script."
+  Write-Output "For details & guide, visit: https://github.com/k3rn31p4nic/appveyor-discord-webhook"
+  Exit
+}
+
+Write-Output "[Webhook]: Sending webhook to Discord..."
+
+Switch ($STATUS) {
+  "success" {
+    $EMBED_COLOR=3066993
+    $STATUS_MESSAGE="Passed"
+    Break
+  }
+  "failure" {
+    $EMBED_COLOR=15158332
+    $STATUS_MESSAGE="Failed"
+    Break
+  }
+  default {
+    Write-Output "Default!"
+    Break
+  }
+}
+$AVATAR="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Appveyor_logo.svg/256px-Appveyor_logo.svg.png"
+
+if (!$APPVEYOR_REPO_COMMIT) {
+  $APPVEYOR_REPO_COMMIT="$(git log -1 --pretty="%H")"
+}
+
+$AUTHOR_NAME="$(git log -1 "$APPVEYOR_REPO_COMMIT" --pretty="%aN")"
+$COMMITTER_NAME="$(git log -1 "$APPVEYOR_REPO_COMMIT" --pretty="%cN")"
+$COMMIT_SUBJECT="$(git log -1 "$APPVEYOR_REPO_COMMIT" --pretty="%s")"
+$COMMIT_MESSAGE="$(git log -1 "$APPVEYOR_REPO_COMMIT" --pretty="%b")"
+
+if ($AUTHOR_NAME -eq $COMMITTER_NAME) {
+  $CREDITS="$AUTHOR_NAME authored & committed"
+}
+else {
+  $CREDITS="$AUTHOR_NAME authored & $COMMITTER_NAME committed"
+}
+
+if ($APPVEYOR_PULL_REQUEST_NUMBER) {
+  $URL="https://github.com/$APPVEYOR_REPO_NAME/pull/$APPVEYOR_PULL_REQUEST_NUMBER"
+}
+else {
+  $URL=""
+}
+
+$TIMESTAMP="$(Get-Date -format s)Z"
+$WEBHOOK_DATA="{
+  ""username"": """",
+  ""avatar_url"": ""$AVATAR"",
+  ""embeds"": [ {
+    ""color"": $EMBED_COLOR,
+    ""author"": {
+      ""name"": ""Job #$APPVEYOR_JOB_NUMBER (Build #$APPVEYOR_BUILD_NUMBER) $STATUS_MESSAGE - $APPVEYOR_REPO_NAME"",
+      ""url"": ""https://ci.appveyor.com/project/$APPVEYOR_ACCOUNT_NAME/$APPVEYOR_PROJECT_SLUG/build/$APPVEYOR_BUILD_NUMBER"",
+      ""icon_url"": ""$AVATAR""
+    },
+    ""title"": ""$COMMIT_SUBJECT"",
+    ""url"": ""$URL"",
+    ""description"": ""$COMMIT_MESSAGE $CREDITS"",
+    ""fields"": [
+      {
+        ""name"": ""Commit"",
+        ""value"": ""[``$($APPVEYOR_REPO_COMMIT.substring(0, 7))``](https://github.com/$APPVEYOR_REPO_NAME/commit/$APPVEYOR_REPO_COMMIT)"",
+        ""inline"": true
+      },
+      {
+        ""name"": ""Branch/Tag"",
+        ""value"": ""[``$APPVEYOR_REPO_BRANCH``](https://github.com/$APPVEYOR_REPO_NAME/tree/$APPVEYOR_REPO_BRANCH)"",
+        ""inline"": true
+      }
+    ],
+    ""timestamp"": ""$TIMESTAMP""
+  } ]
+}"
+
+Invoke-RestMethod -Uri "$WEBHOOK_URL" -Method "POST" -UserAgent "AppVeyor-Webhook" `
+  -ContentType "application/json" -Header @{"X-Author"="k3rn31p4nic#8383"} `
+  -Body $WEBHOOK_DATA
+
+Write-Output "[Webhook]: Successfully sent the webhook."
